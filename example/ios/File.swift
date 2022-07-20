37dfe0330b273cdd28da8d07e2104fcc32775b38
Soubhratra-at-238452966179a <soubhratra@instnt.org>
2022-07-19 20:02:17 -0400

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  InstntReactNativeSdkExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    '@instnt/instnt-react-native-sdk': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
